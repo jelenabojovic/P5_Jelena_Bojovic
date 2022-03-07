2263bdfa0aed5106ef3037296e8527da0adc40d0
Jelena Bojovic <jelenabojovic981@gmail.com>
2022-03-07 15:39:41 +0100

    get data

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,18 @@
 
-fetch ('http://localhost:3000/api/products')
-.then ((response) => response.json ())
-.then ((data) => console.log (data))
+fetch("http://localhost:3000/api/products")
+.then((response) => response.json())
+.then((data) => addProducts(data))
+
+// added products
+function addProducts(data) {
+    let imageUrl= data[0].imageUrl
+
+let anchor = document.createElement ("a")
+anchor.href = imageUrl
+anchor.text ="un super canape"
+
+const items = document.getElementById ('items')
+if (items != null) {
+    items.appendChild(anchor)
+   }
+}
