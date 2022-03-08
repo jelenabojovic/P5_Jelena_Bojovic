1f03a19019610b384cff29c1df62baba8ace5302
Jelena Bojovic <jelenabojovic981@gmail.com>
2022-03-08 13:28:46 +0100

    added products to the homepage

--- front/js/script.js
+++ front/js/script.js
@@ -1,18 +1,48 @@
+//get products from API
+getProducts();
 
-fetch("http://localhost:3000/api/products")
-.then((response) => response.json())
-.then((data) => addProducts(data))
+//add products to the home page
+addProducts();
 
-// added products
-function addProducts(data) {
-    let imageUrl= data[0].imageUrl
-
-let anchor = document.createElement ("a")
-anchor.href = imageUrl
-anchor.text ="un super canape"
-
-const items = document.getElementById ('items')
-if (items != null) {
-    items.appendChild(anchor)
-   }
+ async function getProducts() {
+   let products = await fetch("http://localhost:3000/api/products");
+   console.log("the products have been added")
+   return products.json();
 }
+
+async function addProducts () {
+    let result = await getProducts ()
+    .then ((product) => {
+        for (let i=0; i < product.length; i++) {
+
+            // creation of the link
+            let anchor= document.createElement ("a");
+            document.getElementById('items').appendChild(anchor);
+            anchor.href = "./product.html?id=${product[i]._id}" ;
+
+            //creation of the article
+            let productArticle = document.createElement("article");
+            anchor.appendChild(productArticle);
+
+            //creation of the image
+            let productImage = document.createElement ("img");
+            productArticle.appendChild(productImage);
+            productImage.src = product[i].imageUrl;
+            productImage.alt = product[i].altTxt;
+
+            //creation of the title h3
+            let productName = document.createElement("h3");
+            productArticle.appendChild(productName);
+            productName.classList.add("productName");
+            productName.innerHTML = product[i].name;
+
+            //creation of the paragraph description
+            let productDescription = document.createElement ("p");
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add("productName");
+            productDescription.innerHTML = product[i].description;
+        }
+    });
+    console.log("the products are done")
+}
+
