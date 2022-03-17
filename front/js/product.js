08a334238a956d3d0c3fe9791c81ace3191b5e88
Jelena Bojovic <jelenabojovic981@gmail.com>
2022-03-17 14:02:43 +0100

    added products to the cart and creation of local storage

--- front/js/product.js
+++ front/js/product.js
@@ -1,40 +1,128 @@
-// Get product Id 
+//// récupération de l'id du produit
 let params = (new URL(document.location)).searchParams;
 const id = params.get('id');
+let product = "";
 
-//Create product and its description
 
-let itemImage = document.querySelector(".item__img");
-let img = document.createElement("img");
-itemImage.appendChild(img);
-let itemTitle = document.getElementById("title");
-let itemPrice = document.getElementById("price");
-let itemDescription = document.getElementById("description");
-let itemColors = document.getElementById("colors");
+getProduct();
 
-// Get a unique product by its ID
 
-getItem ();
+// Récupération des articles de l'API
+function getProduct() {
+    fetch(`http://localhost:3000/api/products//${id}`)
+    .then((res) => {
+        return res.json();
+    })
 
-async function getItem () {
-    await fetch(`http://localhost:3000/api/products/${id}`)
-    .then((response) => response.json())
-    .then (item => {
-     itemImage.src = item.imageUrl;
-     itemImage.alt = item.altTxt;
-     itemTitle.innerHTML = item.name;
-     itemPrice.innerHTML = item.price;
-     itemDescription.innerText = item.description;
-     document.title = item.title;
 
-     let itemColors = document.getElementById("colors");
-     for(let i=0; i < item.colors.length; i++) {
-         let option = document.createElement("option");
-         option.innerText = item.colors [i];
-         itemColors.appendChild(option);
-     }
+    // Répartition des données de l'API dans le DOM
+    .then(async function (resultsAPI) {
+        product = await resultsAPI;
+         if (product){
+            getPost(product);
+        }
     })
 }
+    
+function getPost(product){
+    // insertion des information du produit
+    let itemImg = document.createElement("img");
+    document.querySelector(".item__img").appendChild(itemImg);
+    itemImg.src = product.imageUrl;
+    itemImg.alt = product.altTxt;
+
+    let itemName = document.getElementById('title');
+    itemName.innerHTML = product.name;
+
+    let itemPrice = document.getElementById('price');
+    itemPrice.innerHTML = product.price;
+
+    let itemDescription = document.getElementById('description');
+    itemDescription.innerHTML = product.description;
+
+
+    // Insertion des options de couleurs
+    let productColors = document.getElementById("colors");
+   for (let i=0; i < product.colors.length; i++) {
+   let option = document.createElement("option");
+   option.innerText = product.colors [i];
+   productColors.appendChild(option);
+}
+
+    addToCart(product);
+}
+
+
+//Gestion du panier
+function addToCart(product) {
+    const addToCartBtn = document.querySelector("#addToCart");
+    const color = document. querySelector("#colors");
+   const quantity = document.querySelector("#quantity");
+
+
+
+    //Ecouter le panier avec 2 conditions couleur non nulle et quantité entre 1 et 100
+    addToCartBtn.addEventListener("click", ()=>{
+        if (color.value !== null || quantity.value > 0){
+
+
+    //Recupération du choix de la couleur
+    let colorChoice = color.value;
+                
+    //Recupération du choix de la quantité
+    let quantityChoice = quantity.value;
+
+
+    //créer un nouveau produit à ajouter au panier
+    let articleInCart = {
+        articleId: id,
+        articleColor: colorChoice,
+        articleQuantity: quantityChoice,
+        articleName: product.name,
+        articlePrice: product.price,
+        articleDescription: product.description,
+        articleImg: product.imageUrl,
+        articleAltImg: product.altTxt
+    };
+
+
+    //Initialisation du local storage
+    let cart = JSON.parse(localStorage.getItem("article"));
+
+
+    //Importation dans le local storage
+    //verification si l'article n'existe dèjà dans le panier
+    if (cart) {
+    const articleExist = cart.find(
+        (el) => el.articleId === id && el.articleColor === colorChoice);
+        //S'il est déjà present, incrémente seulement la quantité
+        if (articleExist) {
+            let newQuantite =
+            parseInt(articleInCart.articleQuantity) + parseInt(articleExist.articleQuantity);
+            articleExist.articleQuantity = newQuantite;
+            localStorage.setItem("article", JSON.stringify(cart));
+           
+        //Si le produit commandé n'est pas dans le panier
+        } else {
+            cart.push(articleInCart);
+            localStorage.setItem("article", JSON.stringify(cart));
+            
+        }
+    //Si le panier est vide
+    } else {
+        cart =[];
+        cart.push(articleInCart);
+        localStorage.setItem("article", JSON.stringify(cart));
+        
+    }}
+    });
+}
+
+
+    
+    
+
+
 
 
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,42 +1,44 @@
-//get products from API
+
 getProducts();
 
-//add products to the homepage
+
 addProducts();
 
+// Récuperation de produits d'API
  async function getProducts() {
    let products = await fetch("http://localhost:3000/api/products");
    return products.json();
 }
 
+// Creation de produits dans le page d'accueil
 async function addProducts () {
     let result = await getProducts ()
     .then ((product) => {
         for (let i=0; i < product.length; i++) {
             
 
-            // creation of the link
+            // insertion de l'élément "a"
             let anchor = document.createElement ("a");
             document.getElementById('items').appendChild(anchor);
             anchor.href = `product.html?id=${product[i]._id}`;
 
-            //creation of the article
+            //insertion de l'élément "article"
             let productArticle = document.createElement("article");
             anchor.appendChild(productArticle);
 
-            //creation of the image
+            //insertion de l'image
             let productImage = document.createElement ("img");
             productArticle.appendChild(productImage);
             productImage.src = product[i].imageUrl;
             productImage.alt = product[i].altTxt;
 
-            //creation of the title h3
+            //insertion du titre h3
             let productName = document.createElement("h3");
             productArticle.appendChild(productName);
             productName.classList.add("productName");
             productName.innerHTML = product[i].name;
 
-            //creation of the paragraph description
+            //insertion de la description "p"
             let productDescription = document.createElement ("p");
             productArticle.appendChild(productDescription);
             productDescription.classList.add("productName");
